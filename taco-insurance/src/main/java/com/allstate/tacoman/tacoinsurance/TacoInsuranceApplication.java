804d619617a16cc0352f19a3565cdecf9214af14
Rioh <RiohRowe@gmail.com>
2021-07-28 23:40:21 +0000

    initial project and configuration

---
+++ taco-insurance/src/main/java/com/allstate/tacoman/tacoinsurance/TacoInsuranceApplication.java
@@ -0,0 +1,13 @@
+package com.allstate.tacoman.tacoinsurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoInsuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoInsuranceApplication.class, args);
+	}
+
+}
---
+++ taco-insurance/src/test/java/com/allstate/tacoman/tacoinsurance/TacoInsuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allstate.tacoman.tacoinsurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoInsuranceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
